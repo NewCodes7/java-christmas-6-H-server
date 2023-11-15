97844220fda096be1bb3c503d83178dcfa6f0f87
H-server <hani103201@naver.com>
2023-11-15 20:25:23 +0900

    refactor: enum 요소 이름 변경

--- src/main/java/christmas/constant/event/DiscountType.java
+++ src/main/java/christmas/constant/event/DiscountType.java
@@ -4,7 +4,7 @@ import christmas.model.customer.VisitDate;
 
 public enum DiscountType {
     CHRISTMAS_DEAL("크리스마스 디데이 할인"),
-    WEEKDAY_DEAL("평일 할인"),
+    WEEK_DEAL("평일 할인"),
     SPECIAL_DEAL("특별 할인"),
     GIFT_EVENT("증정 이벤트");
 
@@ -16,7 +16,7 @@ public enum DiscountType {
     }
 
     public String getDisplayName() {
-        if (this == WEEKDAY_DEAL && VisitDate.isWeekend()) {
+        if (this == WEEK_DEAL && VisitDate.isWeekend()) {
             return "주말 할인" + ": ";
         }
         return displayName + ": ";
