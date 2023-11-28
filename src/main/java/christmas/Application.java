8727edfa5287b666f629badc83f1068d96de7808
김상혁 <hani103201@naver.com>
2023-11-28 20:22:45 +0900

    Revert "[크리스마스] 김상혁 리뷰 요청"

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,13 +1,7 @@
 package christmas;
 
-import christmas.constant.Message;
-import christmas.controller.MainController;
-import christmas.view.OutputView;
-
 public class Application {
     public static void main(String[] args) {
-        OutputView.print(Message.WELCOME.getMessage());
-        MainController mainController = new MainController();
-        mainController.excute();
+        // TODO: 프로그램 구현
     }
 }
